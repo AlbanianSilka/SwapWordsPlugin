843da57766d6467e1737420bb3411d2035c9d293
Andrii Khavriuta <58111574+AlbanianSilka@users.noreply.github.com>
2021-09-16 12:50:52 +0300

    Updated swap_words_service.rb
     I've refactored swap_words_service - removed unnecessary 'require' statements, made the whole method of swap_words containing less lines and now hash collecting is in only one line.

--- notebook/app/services/swap_service.rb
+++ notebook/app/services/swap_service.rb
@@ -3,39 +3,28 @@
 # Service for swap words def
 class SwapService
   class << self
-  include ActionController::Helpers
-  include ActionController::Flash
 
-  def initialize(post)
-    @post = post
-  end
+    def initialize(post)
+      @post = post
+    end
 
-  def execute(string)
-      all_keys = KeynoteStore.get_keynotes.values
-      keys_arr = all_keys.map{|x| x[:key]}
-      links_arr = all_keys.map{|x| x[:link]}
-      all_keys = Hash[keys_arr.zip(links_arr)]
-      all_keys = all_keys.transform_keys(&:downcase)
-      all_keys.each do |key, value|
-        count_links = string.to_s.scan(%r{<a href='#{value}'>#{key}</a>}i).size
-        next if count_links >= 2
+    def execute(string)
+      # creating a hash to collect all of the keynotes in right format
+      all_keynotes = Hash[(KeynoteStore.get_keynotes.values.map{|x| x[:key]}).zip(KeynoteStore.get_keynotes.values.map{|x| x[:link]})].transform_keys(&:downcase)
 
-        if count_links == 1
-          if string.to_s.scan(/#{key}\s|\s#{key}|\s#{key}\s/i).size.positive?
-            string = string.to_s.sub(/#{key}\s|\s#{key}|\s#{key}\s/i) do
-              "<a href='#{value}'>#{key.capitalize}</a>"
-            end
-          end
-        else
-          string = string.to_s.sub(/#{key}\s|\s#{key}|\s#{key}\s/i) do
-            " <a href='#{value}'>#{key.capitalize}</a>"
-          end
-          string = string.to_s.sub(/#{key}\s|\s#{key}|\s#{key}\s/i) do
-            " <a href='#{value}'>#{key.capitalize}</a>"
+      string.split
+               .chunk(&:downcase)
+               .flat_map do |link,a|
+        a.flat_map.with_index do |key,i|
+          # 'if' statement created to limit creating hyperlinks for each of the keywords from hash in 2
+          if i < 2 && all_keynotes.key?(link)
+            "<a href = #{all_keynotes[link]}>#{key}</a>"
+          else
+            key
           end
         end
-      end
-      string
-  end
+      end.join(' ')
+    end
+
   end
 end
